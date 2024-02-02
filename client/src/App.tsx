b3d1da8b9a660ff986751ebfe7ac5e161720e8a1
bilal-kun <khawajabilal902@gmail.com>
2024-02-02 21:21:28 +0500

    added dashboard pages along with dashboard sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,23 +8,32 @@ import useAuthStore from "./store/authStore";
 import PostJob from "./pages/PostJob";
 import Jobs from "./pages/Jobs";
 import Job from "./pages/Job";
+import Dashboard from "./pages/Dashboard";
+import Jobposts from "./components/Dashboard/JobPosts/Jobposts";
+import DashboardHome from "./components/Dashboard/Home/DashboardHome";
+import Candidates from "./components/Dashboard/Candidates/Candidates";
 
 const App = () => {
   const { user } = useAuthStore();
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={user ? <Home /> : <Login />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="user/:id" element={user ? <Profile /> : <Login />} />
-        <Route path="post-job" element={<PostJob />} />
-        <Route path="jobs" element={<Jobs />} />
-        <Route path="job/:id" element={<Job />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={user ? <Home /> : <Login />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="user/:id" element={user ? <Profile /> : <Login />} />
+          <Route path="post-job" element={<PostJob />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="job/:id" element={<Job />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/job-posts" element={<Jobposts />} />
+          <Route path="/candidates" element={<Candidates />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
